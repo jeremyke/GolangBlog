aecfa3efc4dba2dd20d69220d00041acdcd239c7
jeremyke <lipengke@xiaoe-tech.com>
2020-09-13 23:15:16 +0800

    复习一遍，接着来

---
+++ src/go_code/chapter01/main/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main1() {
+	var a byte = 'n'
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%c\n", a)
+	fmt.Printf("%v\n", a)
+	b := fmt.Sprintf("%c\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%s", b)
+}
+
+func main() {
+	var str1 string = "hello"
+	var str2 string = "123"
+	int1, _ := strconv.ParseInt(str1, 10, 64)
+	int2, _ := strconv.ParseInt(str2, 10, 64)
+	fmt.Printf("int1's type %T int1=%v\n", int1, int1) // string hello无法转为整数，只能设置位0值
+	fmt.Printf("int2's type %T int2=%v\n", int2, int2)
+}
