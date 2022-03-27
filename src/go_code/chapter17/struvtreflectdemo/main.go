7f3e6938a37205f10d805c0899c33fa87e35b290
jeremyke <2287111230@qq.com>
2022-03-28 00:38:58 +0800

    golang反射-290

--- src/go_code/chapter17/struvtreflectdemo/main.go
+++ src/go_code/chapter17/struvtreflectdemo/main.go
@@ -13,15 +13,15 @@ type Stu struct {
 func reflectTest(b interface{}) {
 	//获取reflect.Type
 	rType := reflect.TypeOf(b)
-	fmt.Println("rType=", rType) //int
+	fmt.Println("rType=", rType) //Stu
 
 	//获取reflect.Value
 	rValue := reflect.ValueOf(b)
-	fmt.Printf("rValue=%v 类型为：%T\n", rValue, rValue) //100 reflect.Value
+	fmt.Printf("rValue=%v 类型为：%T\n", rValue, rValue) //rValue={tom 20} 类型为：reflect.Value
 
 	//获取变量对应的类别常量rValue.kind() 或者 rType.kind()
-	kind1 := rType.Kind()
-	kind2 := rValue.Kind()
+	kind1 := rType.Kind()                           //struct
+	kind2 := rValue.Kind()                          //struct
 	fmt.Printf("kind1=%v kind2=%v\n", kind1, kind2) //struct
 
 	//将reflect.Value-->interface{}
